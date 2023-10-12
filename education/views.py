63e2f4a8699207a0542ebdfd8a275091e741c0a3
anastasia <nastia.riabtseva18@gmail.com>
2023-10-12 17:48:23 +0300

    Commented out the conditions in the view for checking in Postman and corrected the URL name

--- education/views.py
+++ education/views.py
@@ -8,38 +8,38 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
-    def perform_create(self, serializer):
-        course = serializer.save()
-        course.author = self.request.user
-        course.save()
+    # def perform_create(self, serializer):
+    #     course = serializer.save()
+    #     course.author = self.request.user
+    #     course.save()
 
-    def list(self, request):
-        if self.request.user.is_manager:
-            queryset = Course.objects.all()
-        else:
-            queryset = Course.objects.filter(author=self.request.user)
-        return queryset
+    # def list(self, request):
+    #     if self.request.user.is_manager:
+    #         queryset = Course.objects.all()
+    #     else:
+    #         queryset = Course.objects.filter(author=self.request.user)
+    #     return queryset
 
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
 
-    def perform_create(self, serializer):
-        lesson = serializer.save()
-        lesson.author = self.request.user
-        lesson.save()
+    # def perform_create(self, serializer):
+    #     lesson = serializer.save()
+    #     lesson.author = self.request.user
+    #     lesson.save()
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
-    def get_queryset(self):
-        if self.request.user.is_moderator:
-            queryset = Lesson.objects.all()
-        else:
-            queryset = Lesson.objects.filter(author=self.request.user)
-        return queryset
+    # def get_queryset(self):
+    #     if self.request.user.is_moderator:
+    #         queryset = Lesson.objects.all()
+    #     else:
+    #         queryset = Lesson.objects.filter(author=self.request.user)
+    #     return queryset
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
